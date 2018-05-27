5455be2bc5814734fec2c9fea94a1d62ed016ad1
n0rthf4ce <eric.liu85@gmail.com>
2018-05-26 23:34:52 -0700

    move logic to server side

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,30 @@
 var friends=require("../data/friends");
+var diff = 10000, newDiff, bestFriendName, bestFriendURL;
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
     app.post("/api/friends", function (req, res) {
+        const NEW_SCORES = req.body["scores[]"];
+
+        friends.forEach(function (user) {
+            newDiff = 0;
+            const SCORES = user["scores[]"];
+            for (let i = 0; i < SCORES.length; i++) {
+                newDiff += Math.abs(SCORES[i] - NEW_SCORES[i]);
+            };
+            if (diff > newDiff) {
+                diff = newDiff;
+                bestFriendName = user.name;
+                bestFriendURL = user.photo;
+            }
+        });
+        console.log(diff, bestFriendName, bestFriendURL);
         friends.push(req.body);
-        res.json(friends);
+        res.json({
+            name:bestFriendName,
+            url: bestFriendURL
+        });
     });
 }
