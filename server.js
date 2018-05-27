bf4bfc55b4fb44ca4ea81abff455ad90f63052b8
n0rthf4ce <eric.liu85@gmail.com>
2018-05-27 00:15:03 -0700

    change port form const to var

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-const PORT = process.env.port || 3000;
+var PORT = process.env.port || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
